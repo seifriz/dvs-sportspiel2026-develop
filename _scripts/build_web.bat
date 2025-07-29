aab2e89f54968770dd61c889fb7286aa5901d7fe
seifriz <seifriz@dshs.koeln.de>
2025-07-29 19:04:38 +0200

    build_web.bat with robocopy

--- _scripts/build_web.bat
+++ _scripts/build_web.bat
@@ -31,38 +31,11 @@ echo.
 choice /c JN /n /m "Bitte bestätigen Sie mit J (Ja) oder N (Nein): "
 
 if errorlevel 2 goto :abbruch
-if errorlevel 1 goto :kopieren
+if errorlevel 1 goto :sync
 
-:kopieren
-:: Produktiv-Version im 'dvs-sportspiel2026'-Verzeichnis löschen...
-:: rd /s /q "%WEB_DIR%"
-rd /s /q "%WEB_DIR%"\css
-rd /s /q "%WEB_DIR%"\images
-rd /s /q "%WEB_DIR%"\js
-rd /s /q "%WEB_DIR%"\node_modules
-rd /s /q "%WEB_DIR%"\scss
-rd /s /q "%WEB_DIR%"\video
-
-:: Neue leere Verzeichnisstruktur aufbauen...
-mkdir "%WEB_DIR%"\css\font
-mkdir "%WEB_DIR%\images\keynotes"
-mkdir "%WEB_DIR%\js"
-mkdir "%WEB_DIR%\node_modules"
-mkdir "%WEB_DIR%\scss"
-mkdir "%WEB_DIR%\video"
-
-:: Kopiere Dateien von 'dvs-sportspiel2026-develop' nach 'dvs-sportspiel2026' mit Originaldatum...
-
-:: noch so machen, das in "%WEB_DIR%\" .git und .idea und readme.md erhaltenbleibt!!!!!
-
-::xcopy "%DEV_DIR%\" "%WEB_DIR%\" /i /y
-
-xcopy "%DEV_DIR%\css" "%WEB_DIR%\css" /s /e /i /y
-xcopy "%DEV_DIR%\images" "%WEB_DIR%\images" /s /e /i /y
-xcopy "%DEV_DIR%\js" "%WEB_DIR%\js" /s /e /i /y
-xcopy "%DEV_DIR%\node_modules" "%WEB_DIR%\node_modules" /s /e /i /y
-xcopy "%DEV_DIR%\scss" "%WEB_DIR%\scss" /s /e /i /y
-xcopy "%DEV_DIR%\video" "%WEB_DIR%\video" /s /e /i /y
+:sync
+:: Develop-Version synchronisieren in Produktiv-Version (mit Ausnahmen)...
+robocopy "%DEV_DIR%" "%WEB_DIR%" /MIR /XF .gitignore README.md /XD .git .idea /R:1 /W:1
 
 echo Build-Web abgeschlossen!
 
